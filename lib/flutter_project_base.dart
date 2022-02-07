087392a426af40974677e31a6ae12ecdfa0db581
BurakAkten <burak_akten@hotmail.com>
2022-02-07 14:55:17 +0300

    Flutter project generic view and viewmodel created

---
+++ lib/flutter_project_base.dart
@@ -0,0 +1,2 @@
+export './src/viewmodels/base_viewmodel.dart';
+export './src/views/base_view.dart';
---
+++ lib/src/views/base_view.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_project_base/src/viewmodels/base_viewmodel.dart';
+import 'package:provider/provider.dart';
+
+class BaseView<T extends BaseViewModel> extends StatefulWidget {
+  final T Function(BuildContext)? vmBuilder;
+  final Widget Function(BuildContext, T)? builder;
+
+  const BaseView({Key? key, required this.vmBuilder, required this.builder})
+      : assert(vmBuilder != null, builder != null),
+        super(key: key);
+
+  @override
+  _BaseViewState createState() => _BaseViewState<T>();
+}
+
+class _BaseViewState<T extends BaseViewModel> extends State<BaseView<T>> {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<T>.value(
+      value: widget.vmBuilder!(context),
+      child: Consumer<T>(
+        builder: _buildScreenContent,
+      ),
+    );
+  }
+
+  Widget _buildScreenContent(
+          BuildContext context, T viewModel, Widget? child) =>
+      !viewModel.isInitialized
+          ? const Center(child: CircularProgressIndicator())
+          : Stack(
+              children: [
+                widget.builder!(context, viewModel),
+                Visibility(
+                  visible: viewModel.isLoading,
+                  child: const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                )
+              ],
+            );
+}
